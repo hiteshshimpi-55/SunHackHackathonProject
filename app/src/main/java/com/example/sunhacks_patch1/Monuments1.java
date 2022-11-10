242e06f222498df6100d9b07112e8fb552f6146d
Admin <sanikasonawane227@gmail.com>
2022-11-11 01:09:32 +0700

    Monuments and Forts

--- app/src/main/java/com/example/sunhacks_patch1/Monuments1.java
+++ app/src/main/java/com/example/sunhacks_patch1/Monuments1.java
@@ -2,20 +2,13 @@ package com.example.sunhacks_patch1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class Monuments1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_monuments1);
     }
 }
